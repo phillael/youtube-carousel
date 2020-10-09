fe0e4e2d515787fbb7483e6c01fe4f9d066ad81c
Phillip Aelony <phillael@yahoo.com>
2020-10-09 15:01:52 -0500

    Updates

---
+++ precache-manifest.3bb8189aa01480d0269fc06a59f2ccb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7953a1153d22dc8907985503de60b6",
+    "url": "/youtube-carousel/index.html"
+  },
+  {
+    "revision": "807fa0c9bfb4cc3caab8",
+    "url": "/youtube-carousel/static/css/main.a836ab3e.chunk.css"
+  },
+  {
+    "revision": "52357af8697ad28872c0",
+    "url": "/youtube-carousel/static/js/2.c12eb56f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-carousel/static/js/2.c12eb56f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "807fa0c9bfb4cc3caab8",
+    "url": "/youtube-carousel/static/js/main.f4496ac8.chunk.js"
+  },
+  {
+    "revision": "b590f71cda48a37359f7",
+    "url": "/youtube-carousel/static/js/runtime-main.f6f36002.js"
+  }
+]);
